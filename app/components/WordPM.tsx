4e3a700df8bddd40732ef583b1ce1eef8cae28c2
jalazer <j.almanzar@northeastern.edu>
2025-09-22 22:47:19 -0400

    Added The excerpt to text!

--- app/components/WordPM.tsx
+++ app/components/WordPM.tsx
@@ -1,8 +1,36 @@
+"use client"
+
+import wpmText from "../mockData/wpmText.json";
+
+import { useState } from "react";
 
 export default function WPM() {
+
+    const [userTxt, setUserTxt] = useState('');
+
     return (
         <>
-            <h1>Hello there!</h1>
+            <div className="container-hold">
+                <div className="text-hold">
+                {wpmText[0]}
+                </div>
+                <div className="type cheker">
+                    <input 
+                        type="text"
+                        value={userTxt}
+                        onChange={(event) => {
+                            setUserTxt(event.target.value);
+                            alert("changed!")}}
+                        onKeyDown={(event) => {
+                            if (event.key === "Enter") {
+                                alert("Entered!")
+                            }
+                        }}/>
+                </div>
+                <h1>{userTxt}</h1>
+            </div>
+            
+            
         </>
     )
 }
